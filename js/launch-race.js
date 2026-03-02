d73dda67d2879957b6df74eb146f9b774c495f6c
bcordry <145066989+bcordry@users.noreply.github.com>
2026-03-02 14:52:54 -0700

    Update raceData deaths for drayano

--- js/launch-race.js
+++ js/launch-race.js
@@ -180,7 +180,7 @@ const raceData = {
     kindle:       { progress: 13, deaths: 1 },
     flygonhg:     { progress: 10, deaths: 1 },
     kyacolosseum: { progress: 12, deaths: 0 },
-    drayano:      { progress: 12, deaths: 4 },
+    drayano:      { progress: 13, deaths: 5 },
     buhrito:      { progress: 13, deaths: 1 }
 };
 
@@ -346,6 +346,7 @@ document.addEventListener('DOMContentLoaded', () => {
     updateRaceTracker();   // Set all token positions and death counts from raceData
 
 });
+
 
 
 
