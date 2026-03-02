38b7c134475b1360f971eb5efa0a146b1af10cc7
bcordry <145066989+bcordry@users.noreply.github.com>
2026-03-02 14:51:15 -0700

    Update race progress for flygonhg

--- js/launch-race.js
+++ js/launch-race.js
@@ -178,7 +178,7 @@ if (cdDays && cdHours && cdMinutes && cdSeconds && cdWrapper) {
  */
 const raceData = {
     kindle:       { progress: 13, deaths: 1 },
-    flygonhg:     { progress: 8, deaths: 0 },
+    flygonhg:     { progress: 10, deaths: 1 },
     kyacolosseum: { progress: 12, deaths: 0 },
     drayano:      { progress: 12, deaths: 4 },
     buhrito:      { progress: 13, deaths: 1 }
@@ -346,6 +346,7 @@ document.addEventListener('DOMContentLoaded', () => {
     updateRaceTracker();   // Set all token positions and death counts from raceData
 
 });
+
 
 
 
